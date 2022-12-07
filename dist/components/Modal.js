e70dcf5262bb3275c75194919255bafada662ebb
Philippe Joubard <philippe.joubard@sfr.fr>
2022-12-07 19:06:25 +0100

    removed console.log

--- dist/components/Modal.js
+++ dist/components/Modal.js
@@ -14,16 +14,16 @@ var _templateObject, _templateObject2, _templateObject3, _templateObject4;
 //border-color: rgb(147, 173, 24, 0.8);
 //background-color: rgb(147, 173, 24, 0.8);
 
-var Button = _styledComponents.default.button(_templateObject || (_templateObject = (0, _taggedTemplateLiteral2.default)(["\ndisplay: block;\nwidth: 100%;\npadding: 8px;\nfont-size: 1rem;\nfont-weight: bold;\nmargin-top: 1rem;\nborder-color: ", ";\nbackground-color: ", ";\ncolor: ", ";\ncursor: pointer;\n"])), function (props) {
+var Button = _styledComponents.default.button(_templateObject || (_templateObject = (0, _taggedTemplateLiteral2.default)(["\n\tdisplay: block;\n\twidth: 100%;\n\tpadding: 8px;\n\tfont-size: 1rem;\n\tfont-weight: bold;\n\tmargin-top: 1rem;\n\tborder-color: ", ";\n\tbackground-color: ", ";\n\tcolor: ", ";\n\tcursor: pointer;\n"])), function (props) {
   return props.bgColour;
 }, function (props) {
   return props.bgColour;
 }, function (props) {
   return props.Colour;
 });
-var Overlay = _styledComponents.default.div(_templateObject2 || (_templateObject2 = (0, _taggedTemplateLiteral2.default)(["\nposition: absolute;\nwidth: 100%;\nheight: 100%;\nbackground: #333333d3;\n"])));
-var ModalInfo = _styledComponents.default.div(_templateObject3 || (_templateObject3 = (0, _taggedTemplateLiteral2.default)(["\ndisplay: block;\nwidth: 95%;\nheight: auto;\nmax-width: 500px;\nmin-width: 300px;\npadding: 30px;\nbackground: #fff;\nborder-radius: 5px;\nposition: absolute;\ntop: 40%;\nleft: 50%;\ntransform: translate(-50%, -50%);\n"])));
-var Message = _styledComponents.default.p(_templateObject4 || (_templateObject4 = (0, _taggedTemplateLiteral2.default)(["\nmargin-bottom: 10px;\nfont-family: Montserrat, sans-serif;\nfont-size: 1.5rem;\nfont-weight: 500;\ntext-align: center;\nbackground-color: #fbfbfb;\ntext-align: center;\n"])));
+var Overlay = _styledComponents.default.div(_templateObject2 || (_templateObject2 = (0, _taggedTemplateLiteral2.default)(["\n\tposition: absolute;\n\twidth: 100%;\n\theight: 100%;\n\tbackground: #333333d3;\n"])));
+var ModalInfo = _styledComponents.default.div(_templateObject3 || (_templateObject3 = (0, _taggedTemplateLiteral2.default)(["\n\tdisplay: block;\n\twidth: 95%;\n\theight: auto;\n\tmax-width: 500px;\n\tmin-width: 300px;\n\tpadding: 30px;\n\tbackground: #fff;\n\tborder-radius: 5px;\n\tposition: absolute;\n\ttop: 40%;\n\tleft: 50%;\n\ttransform: translate(-50%, -50%);\n"])));
+var Message = _styledComponents.default.p(_templateObject4 || (_templateObject4 = (0, _taggedTemplateLiteral2.default)(["\n\tmargin-bottom: 10px;\n\tfont-family: Montserrat, sans-serif;\n\tfont-size: 1.5rem;\n\tfont-weight: 500;\n\ttext-align: center;\n\tbackground-color: #fbfbfb;\n\ttext-align: center;\n"])));
 
 /**
  * Modal is a React Component that takes in four props, and returns a div popping up on the screen with a message,
@@ -33,7 +33,7 @@ var Message = _styledComponents.default.p(_templateObject4 || (_templateObject4 
  * @prop {function} setDisplay to toggle the display
  * @prop {String} message message to display
  * @prop {Object} params { bgColor, Color, link }
- * 
+ *
  * @returns A React component.
  */
 function Modal(_ref) {
@@ -41,12 +41,9 @@ function Modal(_ref) {
     setDisplay = _ref.setDisplay,
     message = _ref.message,
     params = _ref.params;
-  console.log("Modal: ", display, message, params);
   var navigate = (0, _reactRouterDom.useNavigate)();
   var toggleModal = function toggleModal() {
-    console.log("toggleModal: ", display);
     setDisplay(!display);
-    console.log("toggleModal: ", display);
     if (params.link) navigate(params.link);
   };
   return /*#__PURE__*/_react.default.createElement("div", {
