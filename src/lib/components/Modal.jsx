92df8cc1844884d15ea3ae7524133c8ea8e3c602
Philippe Joubard <philippe.joubard@sfr.fr>
2022-12-07 19:05:11 +0100

    removed console.log

--- src/lib/components/Modal.jsx
+++ src/lib/components/Modal.jsx
@@ -7,48 +7,48 @@ import "./Modal.css";
 //background-color: rgb(147, 173, 24, 0.8);
 
 const Button = styled.button`
-display: block;
-width: 100%;
-padding: 8px;
-font-size: 1rem;
-font-weight: bold;
-margin-top: 1rem;
-border-color: ${props => props.bgColour};
-background-color: ${props => props.bgColour};
-color: ${props => props.Colour};
-cursor: pointer;
+	display: block;
+	width: 100%;
+	padding: 8px;
+	font-size: 1rem;
+	font-weight: bold;
+	margin-top: 1rem;
+	border-color: ${(props) => props.bgColour};
+	background-color: ${(props) => props.bgColour};
+	color: ${(props) => props.Colour};
+	cursor: pointer;
 `;
 
 const Overlay = styled.div`
-position: absolute;
-width: 100%;
-height: 100%;
-background: #333333d3;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #333333d3;
 `;
 
 const ModalInfo = styled.div`
-display: block;
-width: 95%;
-height: auto;
-max-width: 500px;
-min-width: 300px;
-padding: 30px;
-background: #fff;
-border-radius: 5px;
-position: absolute;
-top: 40%;
-left: 50%;
-transform: translate(-50%, -50%);
+	display: block;
+	width: 95%;
+	height: auto;
+	max-width: 500px;
+	min-width: 300px;
+	padding: 30px;
+	background: #fff;
+	border-radius: 5px;
+	position: absolute;
+	top: 40%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `;
 
 const Message = styled.p`
-margin-bottom: 10px;
-font-family: Montserrat, sans-serif;
-font-size: 1.5rem;
-font-weight: 500;
-text-align: center;
-background-color: #fbfbfb;
-text-align: center;
+	margin-bottom: 10px;
+	font-family: Montserrat, sans-serif;
+	font-size: 1.5rem;
+	font-weight: 500;
+	text-align: center;
+	background-color: #fbfbfb;
+	text-align: center;
 `;
 
 /**
@@ -59,18 +59,14 @@ text-align: center;
  * @prop {function} setDisplay to toggle the display
  * @prop {String} message message to display
  * @prop {Object} params { bgColor, Color, link }
- * 
+ *
  * @returns A React component.
  */
-function Modal ({ display, setDisplay, message, params }) {
-	console.log("Modal: ", display, message, params);
-
+function Modal({ display, setDisplay, message, params }) {
 	const navigate = useNavigate();
 
 	const toggleModal = () => {
-		console.log("toggleModal: ", display)
 		setDisplay(!display);
-		console.log("toggleModal: ", display)
 		if (params.link) navigate(params.link);
 	};
 
@@ -79,7 +75,9 @@ function Modal ({ display, setDisplay, message, params }) {
 			<Overlay></Overlay>
 			<ModalInfo>
 				<Message>{message}</Message>
-				<Button bgColour={params.bgColor} Colour={params.Color} onClick={toggleModal}>OK</Button>
+				<Button bgColour={params.bgColor} Colour={params.Color} onClick={toggleModal}>
+					OK
+				</Button>
 			</ModalInfo>
 		</div>
 	);
